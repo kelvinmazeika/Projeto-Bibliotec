70bf73bef7ce7b1d026735aa137fe7768392b145
KELVIN TAGLIAFERRO MAZEIKA <51738204898@info.local>
2026-09-08 14:55:02 -0300

    Finalizado tela de login

--- login/login.js
+++ login/login.js
@@ -1,9 +1,9 @@
 const campoSenha = document.getElementById("password");
 const btnSenha = document.querySelector("#mostrar-senha");
 
-console.log(campoSenha);
+console.log(campoSenha.type);
 console.log(btnSenha);
 
 btnSenha.addEventListener("click", function() {
-    alert("Cliquei no botão!");
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password";
 })
