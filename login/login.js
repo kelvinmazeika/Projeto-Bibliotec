0452b1d14aacf7283140dcd61e92cc0fc4acb6a0
KELVIN TAGLIAFERRO MAZEIKA <51738204898@info.local>
2026-09-08 14:21:31 -0300

    :construction: build: Página de login em andamento

---
+++ login/login.js
@@ -0,0 +1,9 @@
+const campoSenha = document.getElementById("password");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+console.log(campoSenha);
+console.log(btnSenha);
+
+btnSenha.addEventListener("click", function() {
+    alert("Cliquei no botão!");
+})
