a135f051ed6a2ea493265fe360a11dfe24cc3104
KELVIN TAGLIAFERRO MAZEIKA <51738204898@info.local>
2026-09-09 17:25:27 -0300

    :construction: build: JS da pagina de Reservas

--- VisaoADM/ListagemReservas/script.js
+++ VisaoADM/ListagemReservas/script.js
@@ -9,3 +9,51 @@ checkbox.addEventListener('change', function() {
     sibling.classList.replace('retirado', 'status-livro');
   }
 });
+
+const checkbox2 = document.querySelector('#item-2');
+
+checkbox2.addEventListener('change', function() {
+  const sibling = this.previousElementSibling;
+  
+  if (this.checked) {
+    sibling.classList.replace('status-livro', 'retirado');
+  } else {
+    sibling.classList.replace('retirado', 'status-livro');
+  }
+});
+
+const checkbox3 = document.querySelector('#item-3');
+
+checkbox3.addEventListener('change', function() {
+  const sibling = this.previousElementSibling;
+  
+  if (this.checked) {
+    sibling.classList.replace('status-livro', 'retirado');
+  } else {
+    sibling.classList.replace('retirado', 'status-livro');
+  }
+});
+
+const checkbox4 = document.querySelector('#item-4');
+
+checkbox4.addEventListener('change', function() {
+  const sibling = this.previousElementSibling;
+  
+  if (this.checked) {
+    sibling.classList.replace('status-livro', 'retirado');
+  } else {
+    sibling.classList.replace('retirado', 'status-livro');
+  }
+});
+
+const checkbox5 = document.querySelector('#item-5');
+
+checkbox5.addEventListener('change', function() {
+  const sibling = this.previousElementSibling;
+  
+  if (this.checked) {
+    sibling.classList.replace('status-livro', 'retirado');
+  } else {
+    sibling.classList.replace('retirado', 'status-livro');
+  }
+});
