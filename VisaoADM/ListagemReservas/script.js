09469f3e22ec32e03f3215e1bfabf9fe3c20bac2
KELVIN TAGLIAFERRO MAZEIKA <51738204898@info.local>
2026-09-10 17:26:31 -0300

    Desenvolvendo Responsividade da Bibliotec

--- VisaoADM/ListagemReservas/script.js
+++ VisaoADM/ListagemReservas/script.js
@@ -1,5 +1,7 @@
-const checkbox = document.querySelector('#item-1');
+const checkbox = document.querySelectorAll('#item');
 
+
+checkbox.items?.forEach || checkbox.forEach(checkbox => {
 checkbox.addEventListener('change', function() {
   const sibling = this.previousElementSibling;
   
@@ -8,52 +10,5 @@ checkbox.addEventListener('change', function() {
   } else {
     sibling.classList.replace('retirado', 'status-livro');
   }
-});
-
-const checkbox2 = document.querySelector('#item-2');
-
-checkbox2.addEventListener('change', function() {
-  const sibling = this.previousElementSibling;
-  
-  if (this.checked) {
-    sibling.classList.replace('status-livro', 'retirado');
-  } else {
-    sibling.classList.replace('retirado', 'status-livro');
-  }
-});
-
-const checkbox3 = document.querySelector('#item-3');
-
-checkbox3.addEventListener('change', function() {
-  const sibling = this.previousElementSibling;
-  
-  if (this.checked) {
-    sibling.classList.replace('status-livro', 'retirado');
-  } else {
-    sibling.classList.replace('retirado', 'status-livro');
-  }
-});
-
-const checkbox4 = document.querySelector('#item-4');
-
-checkbox4.addEventListener('change', function() {
-  const sibling = this.previousElementSibling;
-  
-  if (this.checked) {
-    sibling.classList.replace('status-livro', 'retirado');
-  } else {
-    sibling.classList.replace('retirado', 'status-livro');
-  }
-});
-
-const checkbox5 = document.querySelector('#item-5');
-
-checkbox5.addEventListener('change', function() {
-  const sibling = this.previousElementSibling;
-  
-  if (this.checked) {
-    sibling.classList.replace('status-livro', 'retirado');
-  } else {
-    sibling.classList.replace('retirado', 'status-livro');
-  }
+  });
 });
